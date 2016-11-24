fc2d8b2ccabcb6187c28267f9b68d61f2e968ea6
Gonéri Le Bouder <goneri@redhat.com>
2016-11-24 10:08:34 -0500

    initial import

---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eux
+
+[ -z "${DCI_CERTIFICATION_ID}" ] && exit 0
+
+mkdir -p ansible/roles
+[ -d ansible/roles/openstack-certification ] || git clone https://github.com/redhat-cip/ansible-role-openstack-certification ansible/roles/openstack-certification
+[ -f /etc/yum.repos.d/epel.repo ] || sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum-config-manager --enable epel
+sudo yum install -y ansible
+sudo yum-config-manager --disable epel
+
+
+echo "---
+- hosts: localhost
+  remote_user: stack
+  become: yes
+  vars:
+    certification_id: ${DCI_CERTIFICATION_ID}
+    plugin_type: blockstorage
+  roles:
+    - openstack-certification
+" > ansible/main.yaml
+
+cd ansible
+
+exec ansible-playbook main.yaml
